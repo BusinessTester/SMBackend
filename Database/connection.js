e48f50f5dd0b9a4df2c3b2ecbe947382205e6f28
BusinessTester <125587480+BusinessTester@users.noreply.github.com>
2023-02-23 20:45:37 +0545

    mongodb edit

--- Database/connection.js
+++ Database/connection.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', true)
 // this line of code has been made a mandatory for the newest version of mongodb and i added this line just as recommended by the 
 // console.log error screen
-mongoose.connect(`mongodb+srv://supreme_emperor:Sunrise111!@cluster0.s3xf8tr.mongodb.net/?retryWrites=true&w=majority`,{
+// mongoose.connect(`mongodb+srv://supreme_emperor:Sunrise111!@cluster0.s3xf8tr.mongodb.net/?retryWrites=true&w=majority`,{
+mongoose.connect(`mongodb+srv://StudyMaterialsNepal:RPyoLHlCEAqPeYdO@studymaterialscluster.bkphpsf.mongodb.net/?retryWrites=true&w=majority`,{
+
     useNewUrlParser : true,
     useUnifiedTopology : true,
 })
